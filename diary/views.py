31735bfa6b331c422d606ea233f347e0ff30cb59
kurosawa <i.kurosawa284@gmail.com>
2021-09-10 14:22:50 +0000

    初期セットアップ(テンプレート登録)

--- diary/views.py
+++ diary/views.py
@@ -1,11 +1,11 @@
 from re import template
-from django.http.response import HttpResponse
-from django.shortcuts import render
 from django.views import generic
 
-# Create your views here.
-def index(request):
-    return render(request, 'diary/index.html')
+#from .forms import InquiryForm
 
 class IndexView(generic.TemplateView):
-    template_name="diary/index.html"
+    template_name="index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    #form_class = InquiryForm
